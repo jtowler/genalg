4266a0e7ebf4df4a8b45bd457fe55f248ed68c66
themother <jacktowleretc@hotmail.com>
2020-05-19 22:40:23 +0100

    chapter 3

---
+++ buontempo/Diffuse/Diffuse/Lib/Air.h
@@ -0,0 +1,13 @@
+//
+//  Air.h
+//  Diffuse
+//
+//  Created by Jack Towler on 14/05/2020.
+//  Copyright © 2020 Jack Towler. All rights reserved.
+//
+
+#ifndef Air_h
+#define Air_h
+
+
+#endif /* Air_h */
---
+++ buontempo/Diffuse/Diffuse/Lib/Particle.h
@@ -0,0 +1,13 @@
+//
+//  Particle.h
+//  Diffuse
+//
+//  Created by Jack Towler on 14/05/2020.
+//  Copyright © 2020 Jack Towler. All rights reserved.
+//
+
+#ifndef Particle_h
+#define Particle_h
+
+
+#endif /* Particle_h */
